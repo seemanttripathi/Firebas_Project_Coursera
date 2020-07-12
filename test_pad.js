cffc4e7bd9d17517e82c4ef2e745f91351ec2b51
Seemant Tripathi <47049137+seemanttripathi@users.noreply.github.com>
2020-07-12 00:58:49 +0000

    From Cloud Desktop Coursera

---
+++ test_pad.js
@@ -0,0 +1,46 @@
+// JavaScript file for the web page "World Peace Issue Tracker"
+// Created by Harrison Kong
+// Copyright (C) Coursera 2020
+
+// Your web app's Firebase configuration
+// REPLACE THIS WITH YOUR CONFIG
+
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+// Paste the web app's configuration above this line
+// Our code starts below
+
+// GET A ROOT REFERENCE to issues HERE (type along)
+
+// Task 3 ------------------------------------------
+
+// rootRef.push ({
+//   description: "Logo does not show up on screen 3",
+//   resolved: "yes",
+//   severity: "minor"
+// });
+//
+// rootRef.push ({
+//   description: "Screen flashes on save",
+//   resolved: "no",
+//   severity: "moderate"
+// });
+
+// Task 6 ------------------------------------------
+
+// var recordRef = firebase.database().ref("issues/change-me");
+//
+// recordRef.update ({
+//    "resolved": "yes"
+// });
+
+// Task 7 ------------------------------------------
+
+// var recordRef = firebase.database().ref("issues/delete-me");
+//
+// recordRef.remove()
+//    .catch(function(error) {
+//      alert("Delete failed: " + error.message)
+//    });
